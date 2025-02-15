3b006432b44227a4e196479b9fe6d7718f7bcb75
Jamie Moriarty <jamiemoriarty@qq.com>
2025-02-15 22:58:24 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,118 @@
-mod app;
+use regex::Regex;
+use tex2typst_rs::command_registry::{parse_custom_macros, CommandRegistry};
+use tex2typst_rs::converter::convert_tree;
+use tex2typst_rs::tex_parser::LatexParser;
+use tex2typst_rs::tex_tokenizer::tokenize;
+use tex2typst_rs::typst_writer::TypstWriter;
+use wasm_bindgen::prelude::*;
+use web_sys::{console, window, Document, Event, HtmlTextAreaElement};
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/512x512.png")[..])
-                    .expect("Failed to load icon"),
-            ),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "eframe template",
-        native_options,
-        Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
-    )
+#[wasm_bindgen]
+struct Worker {
+    tex_parser: LatexParser,
+    command_registry: CommandRegistry,
+    typst_writer: TypstWriter,
+    regex: Regex,
 }
 
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    use eframe::wasm_bindgen::JsCast as _;
-
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        let document = web_sys::window()
-            .expect("No window")
-            .document()
-            .expect("No document");
-
-        let canvas = document
-            .get_element_by_id("the_canvas_id")
-            .expect("Failed to find the_canvas_id")
-            .dyn_into::<web_sys::HtmlCanvasElement>()
-            .expect("the_canvas_id was not a HtmlCanvasElement");
-
-        let start_result = eframe::WebRunner::new()
-            .start(
-                canvas,
-                web_options,
-                Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
-            )
-            .await;
-
-        // Remove the loading text and spinner:
-        if let Some(loading_text) = document.get_element_by_id("loading_text") {
-            match start_result {
-                Ok(_) => {
-                    loading_text.remove();
-                }
-                Err(e) => {
-                    loading_text.set_inner_html(
-                        "<p> The app has crashed. See the developer console for details. </p>",
-                    );
-                    panic!("Failed to start eframe: {e:?}");
-                }
-            }
+#[wasm_bindgen]
+impl Worker {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let tex_parser = LatexParser::new(false, false);
+        let command_registry = CommandRegistry::new();
+        let typst_writer = TypstWriter::new();
+        let regex = Regex::new(r"\\\((.+?)\\\)|(?s)\\\[(.+?)\\\]").unwrap();
+        Worker {
+            tex_parser,
+            command_registry,
+            typst_writer,
+            regex,
         }
-    });
+    }
+
+    #[wasm_bindgen]
+    pub fn work(&mut self, tex: &str) -> Result<String, String> {
+        let mut new = String::with_capacity(tex.len());
+        let mut last_match = 0;
+        let captures: Vec<_> = self.regex.captures_iter(tex).collect();
+        for caps in captures {
+            let m = caps.get(0).unwrap();
+            new.push_str(&tex[last_match..m.start()]);
+            let t = if let Some(inline_math) = caps.get(1) {
+                let typst_math = self.convert_math(inline_math.as_str().trim())?;
+                format!("${}$", typst_math)
+            } else if let Some(display_math) = caps.get(2) {
+                let typst_math = self.convert_math(display_math.as_str().trim())?;
+                format!("$\n{}\n$", typst_math)
+            } else {
+                caps[0].to_string()
+            };
+            new.push_str(t.as_str());
+            last_match = m.end();
+        }
+        new.push_str(&tex[last_match..]);
+        Ok(new)
+    }
+
+    fn convert_math(&mut self, tex: &str) -> Result<String, String> {
+        let tokens = tokenize(tex)?;
+        let expanded_tokens = self.command_registry.expand_macros(&tokens)?;
+
+        let tex_tree = self.tex_parser.parse(expanded_tokens)?;
+        let typst_tree = convert_tree(&tex_tree)?;
+
+        let mut writer = TypstWriter::new();
+        writer.serialize(&typst_tree)?;
+        let typst = writer.finalize()?;
+        Ok(typst)
+    }
+
+    fn register_macros(&mut self, macros: &str) -> Result<usize, String> {
+        self.command_registry.custom_macros.clear();
+        self.command_registry.custom_macro_names.clear();
+        self.command_registry
+            .register_custom_macros(parse_custom_macros(macros)?);
+        Ok(self.command_registry.custom_macros.len())
+    }
 }
+
+fn main() {
+    console_error_panic_hook::set_once();
+
+    let document = window()
+        .and_then(|win| win.document())
+        .expect("Could not access the document");
+    let body = document.body().expect("Could not access document.body");
+    let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
+    body.append_child(text_node.as_ref())
+        .expect("Failed to append text");
+}
+
+#[wasm_bindgen]
+pub fn log_message() {
+    console::log_1(&"Hello from Rust!".into());
+}
+
+const DEFAULT_TEX_CODE: &str = r"Here comes some text
+\[
+    x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
+\]
+
+The following use some custom macros (see below)
+\(\R\)
+
+\(\Arg \Log \Int \ball \disk\)";
+const DEFAULT_CUSTOM_MACROS: &str = r"\newcommand{\N}{\mathbb{N}}
+\newcommand{\Z}{\mathbb{Z}}
+\newcommand{\Q}{\mathbb{Q}}
+\newcommand{\R}{\mathbb{R}}
+\newcommand{\CC}{\mathbb{C}}
+\newcommand{\HH}{\mathbb{H}}
+\newcommand{\T}{\mathbb{T}}
+\newcommand{\Arg}{\operatorname{Arg}}
+\newcommand{\Log}{\operatorname{Log}}
+\newcommand{\ball}{\mathbb{B}}
+\newcommand{\disk}{\mathbb{D}}
+\newcommand{\Int}{\operatorname{Int}}
+";
